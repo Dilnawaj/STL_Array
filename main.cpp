48da0b7ac7effe733d20923bb5202fdccb8764c2
Dilnawaj <48027940+Dilnawaj@users.noreply.github.com>
2021-07-24 09:10:01 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ //Declaration of STL Array where 3 is the size
+//array<int,3>arr;
+
+//Declaration of STL Array and Initialize with 0
+//array<int,20>arr={0};
+
+
+//For Initialize STL array with any Value we use fill function()//
+//array<int,5>arr;
+//arr.fill(10);
+
+
+
+
+//---------------Iterators----------------------
+//begin()->It points to the First element of the Array
+//rbegin()(Reverse Begin)->It points to the last Element of the Array
+//end()->It points at after the last Elements (means it not points  to last Element)
+//rend(Reverse End)->It points at before the first Element
+
+
+/*
+auto->it automatically converts to that data type which type of data is present in the array
+for(auto it=arr.begin();it<arr.end();it++)
+{
+  cout<<*it<<" ";
+}
+
+*/
+
+// finding Array Size
+//arr.size();
+
+
+//For Printing First Element
+//arr.front()
+
+
+//For Printing last Element
+//arr.back();
+
+
+
+}
